69de2ae3b41bf4bb5199d259e68b04d17f432d5f
Masashi <t15519ms@gmail.com>
2020-08-14 23:35:20 +0900

    add: Bean構成ファイル

--- src/main/java/jp/tuyano/spring/data1/App.java
+++ src/main/java/jp/tuyano/spring/data1/App.java
@@ -3,10 +3,10 @@ package jp.tuyano.spring.data1;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
 
@@ -14,13 +14,12 @@ public class App {
 	private static EntityManager manager;
 
 	public static void main(String[] args) {
-		context = new ClassPathXmlApplicationContext("classpath:/bean.xml");
-		LocalContainerEntityManagerFactoryBean factory =
-				context.getBean(LocalContainerEntityManagerFactoryBean.class);
-		manager = factory.getNativeEntityManagerFactory().createEntityManager();
+		context = new AnnotationConfigApplicationContext(MyAppConfig.class);
+		EntityManagerFactory factory = context.getBean(EntityManagerFactory.class);
+		manager = factory.createEntityManager();
 
-		MyPersonDataDao<MyPersonData> dao = new MyPersonDataDaoImpl<MyPersonData>(manager);
-		dao.removeEntity(1L);
+		MyPersonDataDao<MyPersonData> dao =
+				new MyPersonDataDaoImpl<MyPersonData>(manager);
 
 		List<MyPersonData> list = dao.getAllEntity();
 		for (MyPersonData person : list) {
---
+++ src/main/java/jp/tuyano/spring/data1/MyAppConfig.java
@@ -0,0 +1,47 @@
+package jp.tuyano.spring.data1;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class MyAppConfig {
+
+	@Bean
+	public DataSource dataSource() {
+		EmbeddedDatabaseBuilder builder =
+				new EmbeddedDatabaseBuilder();
+		builder.setType(EmbeddedDatabaseType.H2);
+		builder.addScript("script.sql");
+		return builder.build();
+	}
+
+	@Bean
+	public JpaVendorAdapter vendorAdapter() {
+		HibernateJpaVendorAdapter vendorAdapter =
+				new HibernateJpaVendorAdapter();
+		vendorAdapter.setGenerateDdl(true);
+		vendorAdapter.setDatabase(Database.H2);
+		return vendorAdapter;
+	}
+
+	@Bean
+	public EntityManagerFactory entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean factory =
+				new LocalContainerEntityManagerFactoryBean();
+
+		factory.setJpaVendorAdapter(vendorAdapter());
+		factory.setPackagesToScan("jp.tuyano.spring.data1");
+		factory.setDataSource(dataSource());
+		factory.afterPropertiesSet();
+		return factory.getObject();
+	}
+}
